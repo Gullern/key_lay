ef1313efe1dbcaef69313f3bb55dab5f70e29e2b
Jan Gulla <jangu@stud.ntnu.no>
2016-07-28 13:34:15 +0200

    Push local files v3

--- alph.py
+++ alph.py
@@ -1,12 +1,3 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-
-######## Todo #########
-# - Distance
-# - Bad sequences
-# - Double letter
-#######################
-
 # Imports
 from itertools import compress
 
@@ -35,143 +26,162 @@ languages = {
              'dvorak': '|1234567890+\§!"#¤%&/()=?`å,.pyfgcrl\'¨Å;:PYFGCRL*^aoeuidhtns-<AOEUIDHTNS_>øæqjkxbmwvzØÆQJKXBMWVZ'
             }
 
-def load_finger_symbols(languages):
-    get_mask = lambda key: (int(character) for character in ''.join(element * 2 for element in physical_finger_placement[key]))
-    finger_symbols = {language: {key: ''.join(compress(languages[language], get_mask(key))) for key in physical_finger_placement} for language in languages}
-    for language in finger_symbols:
-        finger_symbols[language][space_hand + '0'] += ' '
+def load_finger_chars(languages, physical_finger_placement):
+    # Generate dict with list of characters for each finger
+    get_mask = lambda key: (int(char) for char in ''.join(
+            element * 2 for element in physical_finger_placement[key])
+            )
+    finger_2_chars = {language: {
+                                 finger: ''.join(compress(languages[language], get_mask(finger))) for finger in physical_finger_placement
+                                } 
+                      for language in languages
+                     }
 
-finger_symbols = load_finger_symbols(languages)
+    # Attach space
+    for language in languages:
+        finger_2_chars[language][space_hand + '0'] += ' '
 
-# Counters
+    # Generate reverse: associated finger for each char, makes relevant lookup faster
+    def get_finger_slow(language, char):
+        for finger in finger_2_chars[language]:
+            if (char in finger_2_chars[language][finger]):
+                return finger
+        return 'n'
+
+    char_2_finger = {language: {
+                                char: get_finger_slow(language, char) for char in languages[language] + ' '
+                                """ Remembers to attach space to the language string """
+                               } 
+                     for language in languages
+                    }
+    return finger_2_chars, char_2_finger
+
+# Finger lookup dictionaries
+finger_2_chars, char_2_finger = load_finger_chars(languages, physical_finger_placement)
+
+# Stats counters
 counters = {language: {
                        'char_freq': {},
                        'word_length_freq': {},
-                       'hand_freq': {finger[0]: 0 for finger in physical_finger_placement},
-                       'hand_cont_length_freq': {finger[0]: {} for finger in physical_finger_placement},
-                       'finger_freq': {finger: 0 for finger in physical_finger_placement},
-                       'finger_cont_length_freq': {finger: {} for finger in physical_finger_placement},
-                       'rolls': {finger[0]: {'in': 0, 'out': 0, 'short': 0, 'mixed': 0} for finger in physical_finger_placement}
+                       'hand_freq': {},
+                       'hand_length_freq': {finger[0]: {} for finger in physical_finger_placement},
+                       'finger_freq': {},
+                       'finger_length_freq': {finger: {} for finger in physical_finger_placement},
+                       'rolls': {finger[0]: {} for finger in physical_finger_placement}
                        }
             for language in languages
-            }
+           }
+
+# Functions
+def get_finger(language, char):
+    return char_2_finger[language].get(char, 'n')
+
+def dict_increment(*arg):
+    """ args: language, dictionary, key1, key2, ..., keyn """
+    dict = counters[arg[0]][arg[1]]
+    key = arg[-1]
+    levels = len(arg) - 3
+    current = dict
+    for i in range(levels):
+        current = current[arg[i + 2]]
+    if (key in current):
+        current[key] += 1
+    else:
+        current[key] = 1
+
+def get_roll_direction(index_list):
+    if (len(index_list) < 2):
+        return 'short'
+    is_out = True
+    is_in = True
+    for i in range(len(index_list) - 1):
+        if index_list[i + 1] <= index_list[i]:
+            is_out = False
+        if index_list[i + 1] >= index_list[i]:
+            is_in = False
+    if (not is_out and not is_in):
+        return 'none'
+    return 'in' if is_in else 'out'
+
 
 # Read file
 f = open('input.txt', 'r')
 text = f.read();
 
-# Begin
+# Begin processing
 word_length = 0
 hand_length = 0
 finger_length = 0
 roll_buffer = []
 hand_current = 'n'
 finger_current = 'n'
+previous_char = ''
 
-##################### OBS #################################
-# Currently rewriting to handle multiple languages, etc. 
-# Reach this far
-# It will break
-###########################################################
+for language in languages:
+    for char in text:
+        # Characters and words stats
+        dict_increment(language, 'char_freq', char)
 
-def dict_increment(*arg):
-    dict = arg[0]
-    key = arg[-1]
-    levels = len(arg) - 2
-    current = dict
-    for i in range(levels):
-        current = current[arg[i + 1]]
-    if (key in current):
-        current[key] += 1
-    else:
-        current[key] = 1
-
-def get_finger(char):
-    for key in finger_symbols:
-        if (c in finger_symbols[key]): return key
-    return 'n'
-
-def roll_direction(list):
-    if (len(list) < 2):
-        return 'short'
-    not_out = False
-    not_in = False
-    for i in range(len(list) - 1):
-        if list[i + 1] <= list[i]:
-            not_out = True
-        if list[i + 1] >= list[i]:
-            not_in = True
-    if (not_out and not_in):
-        return 'none'
-    return 'in' if not_out else 'out'
-
-for c in text:
-    # Characters and words
-    dict_increment(char_freq, c)
-
-    if (c in termination_symbols):
-        dict_increment(word_length_freq, word_length)
-        word_length = 0
-    else:
-        word_length += 1
-
-    # Fingers
-    finger_new = get_finger(c)
-    finger_freq[finger_new] += 1
-    if (finger_new != finger_current):
-        dict_increment(finger_length_freq, finger_current, finger_length)
-        finger_current = finger_new
-        finger_length = 1
-    else:
-        finger_length += 1
-
-    # Hands and rolls
-    hand_new = finger_new[0]
-    hand_freq[hand_new] += 1
-
-    if (hand_new != hand_current or c == ' '):
-        if (len(roll_buffer) > 0):
-            rolls[hand_current][roll_direction(roll_buffer)] += 1
-        if (c == ' ' or hand_new == 'n'):
-            roll_buffer = []
+        if (char in termination_symbols):
+            if (word_length > 0):
+                """ Avoid recording empty words, e.g. for '. ' """
+                dict_increment(language, 'word_length_freq', word_length)
+            word_length = 0
         else:
-            roll_buffer = [finger_new[1]]
-    elif (hand_new != 'n'):
-        roll_buffer.append(finger_new[1])
+            word_length += 1
 
-    if (hand_new != hand_current):
-        dict_increment(hand_length_freq, hand_current, hand_length)
+        # Get current finger and hand
+        finger_new = get_finger(language, char)
+        hand_new = finger_new[0]
+
+        # Finger stats
+        dict_increment(language, 'finger_freq', finger_new)
+        if (finger_new != finger_current):
+            dict_increment(language, 'finger_length_freq', finger_current, finger_length)
+            finger_length = 1
+        elif (previous_char != char):
+            """ Avoid multiple counting for repeated characters """
+            finger_length += 1
+
+        # Hands stats
+        dict_increment(language, 'hand_freq', hand_new)
+        if (hand_new != hand_current):
+            dict_increment(language, 'hand_length_freq', hand_current, hand_length)
+            hand_length = 1
+        else:
+            hand_length += 1
+
+        # Rolls stats
+        if (hand_new != hand_current or char == ' '):
+            """ Rolls terminate when switching hands OR at a space """
+            if (len(roll_buffer) > 0):
+                dict_increment(language, 'rolls', hand_current, get_roll_direction(roll_buffer))
+            if (char == ' ' or hand_new == 'n'):
+                roll_buffer = []
+            else:
+                roll_buffer = [finger_new[1]]
+        elif (hand_new != 'n'):
+            roll_buffer.append(finger_new[1])
+        
+        # Prepare for next iteration
+        finger_current = finger_new
         hand_current = hand_new
-        hand_length = 1
-    else:
-        hand_length += 1
 
-    finger_current = finger_new
-    hand_current = hand_new
+for language in languages:
+    print('============' + language + '=============')
+    for stats in counters[language]:
+        print(stats)
+        print(counters[language][stats])
+        print()
+    print('===============================')
 
-
-print(char_freq)
-print()
-print(word_length_freq)
-print()
-print(hand_freq)
-print()
-print(hand_length_freq)
-print()
-print(finger_freq)
-print()
-print(finger_length_freq)
-print()
-print(rolls)
-print()
-print()
-
+"""
 same_finger_count = 0
 for i in finger_length_freq:
     for j in finger_length_freq[i]:
         if j > 1:
             same_finger_count += finger_length_freq[i][j]
 print(same_finger_count)
-
+"""
 
 
